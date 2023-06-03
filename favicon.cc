e33ee3385ff9677c9d768639040a576af65f249a
Jovitta George Eluvathingal <68368259+jovittageorgeeluvathingal@users.noreply.github.com>
2023-06-03 11:06:49 +0530

    Create favicon.cc

---
+++ favicon.cc
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>page title</title>
+    <link rel="icon" type="image/x-icon" href="favicon.ico"> 
+</head>
+<body>
+    <h1> the line </h1>
+</body>
+</html>
